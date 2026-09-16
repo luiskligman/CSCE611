8d2c51125ad0bd113d8beb253eff61de976f45e7
Luis Kligman <luiskligman192837465@gmail.com>
2026-09-16 16:10:56 -0400

    .

--- lab2/lab_hex/simtop.sv
+++ lab2/lab_hex/simtop.sv
@@ -36,7 +36,45 @@ module simtop;
 		.HEX7(HEX7)
 	);
 
-// your code here
+	// simple task to check one HEX output against an expected pattern
+	task check(input [6:0] actual, input[6:0] expected, input string label);
+		actual !== expected 
+		? $display("FAIL: %s expected %b got %b, label, expected, actual") 
+		: $display("PASS: %s", label);
+	endtask
+
+	initial begin
+
+		SW = 18'b0;
+
+		// Check all HEX for 0
+		SW[17:0] = 18'd0;
+		#10; 
+		check(HEX0, 7'b100_0000, "HEX0 = 0");
+		#10; 
+		check(HEX1, 7'b100_0000, "HEX1 = 0");
+		#10; 
+		check(HEX2, 7'b100_0000, "HEX2 = 0");
+		#10; 
+		check(HEX3, 7'b100_0000, "HEX3 = 0");
+		#10; 
+		check(HEX4, 7'b100_0000, "HEX4 = 0");
+
+		// Check all HEX for F
+		SW[17:0] = {18{1'b1}};
+		#10; 
+		check(HEX0, 7'b000_1110, "HEX0 = F");
+		#10; 
+		check(HEX1, 7'b000_1110, "HEX1 = F");
+		#10; 
+		check(HEX2, 7'b000_1110, "HEX2 = F");
+		#10; 
+		check(HEX3, 7'b000_1110, "HEX3 = F");
+		#10; 
+		check(HEX4, 7'b000_1110, "HEX4 = F");
+
+
+	end
 
 endmodule
 
